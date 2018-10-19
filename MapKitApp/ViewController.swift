5fcc1031759a41f9a6dcd8fe0bb743a97e348c21
Gilson Leandro Polito <gilsonpolito@gmail.com>
2018-10-19 20:53:03 -0300

    Initial commit

--- MapKitApp/ViewController.swift
+++ MapKitApp/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let latitude: CLLocationDegrees = 48.399193
+        let longitude: CLLocationDegrees = 9.993341
+        
+        let latDelta:CLLocationDegrees = 0.01
+        let longDelta:CLLocationDegrees = 0.01
+        
+        let theSpan:MKCoordinateSpan = MKCoordinateSpan.init(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        let churchLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let theRegion:MKCoordinateRegion = MKCoordinateRegion.init(center:churchLocation, span:theSpan)
+        self.mapView.setRegion(theRegion, animated: true)
+        //self.theMapView.mapType = .satellite
+        
+        let theUImMinisterAnnotation = MKPointAnnotation()
+        theUImMinisterAnnotation.coordinate = churchLocation
+        theUImMinisterAnnotation.title = "UIm Minister"
+        theUImMinisterAnnotation.subtitle = "A famous church in Germany"
+        self.mapView.addAnnotation(theUImMinisterAnnotation)
     }
-
-
 }
-
